4469ed05f40be815646ea451bdb9428913afa2d1
Aegir <butsko.7@osu.edu>
2012-01-20 12:57:06 -0500

    Icons and navbar should have been with theme, not in site files.

---
+++ navbar/osu-navbar-b-custom.php
@@ -0,0 +1,49 @@
+	<div id="osu-Navbar">
+		<p>
+			<a href="#page-content" id="skip" class="osu-semantic">skip to main content</a>
+		</p>
+		<h2 class="osu-semantic">OSU Navigation Bar</h2>
+
+		<table id="osu-Navbar-Table"><tr>
+		<td id="osu-NavbarBreadcrumb-Spacing">&nbsp;
+			
+		</td>
+		<td id="osu-Navbar-Content">
+			<div id="osu-NavbarBreadcrumb">
+				<p id="osu">
+					<a title="The Ohio State University homepage" href="http://www.osu.edu/">The Ohio State University</a>
+				</p>
+				<?php /* ?>
+				<?php if($custom_crumbs) {
+					foreach($custom_crumbs as $crumb => $link) {
+						print '<p class="site-name">';
+							if($link !== 0) {
+								print '<a href="'.$link.'" title="'.$crumb.'">'.$crumb.'</a>';
+							} else {
+								print $crumb;
+							}
+						print '</p>';
+					}
+				} else { ?>
+					<p id="site-name">
+						<?php print $site_name; ?>
+					</p>
+				<?php } ?>
+				*/ ?>
+			</div>
+			<div id="osu-NavbarLinks">
+				<ul>
+					<li><a href="http://www.osu.edu/help.php" title="OSU Help">Help</a></li>
+					<li><a href="http://buckeyelink.osu.edu/" title="Buckeye Link">Buckeye Link</a></li>
+					<li><a href="http://www.osu.edu/map/" title="Campus map">Map</a></li>
+					<li><a href="http://www.osu.edu/findpeople.php" title="Find people at OSU">Find People</a></li>
+					<li><a href="https://webmail.osu.edu" title="OSU Webmail">Webmail</a></li>
+					<li><a href="http://www.osu.edu/search.php" title="Search Ohio State">Search Ohio State</a></li>
+				</ul>			
+			</div>
+		</td>
+		<td>&nbsp;
+			
+		</td>
+		</tr></table>
+	</div>
